68bfba5c7fc26c35527494716a6c4c8820489793
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-07 15:07:35 -0500

    Se agrego el método constructor

--- SistemaMetro.py
+++ SistemaMetro.py
@@ -34,3 +34,34 @@ class Grafo:
     dfs(self, inicio, objetivo, ruta = [], visitado = set())
         Método que imprime el recorrido BFS de un vértice fuente dado.
     """
+
+    def __init__(self, numero_nodos, dirigido=True):
+        """
+        Este metodo funcionara como el constuctor de la clase Grafo(), recibe el Numero de nodos (m_num_nodos),
+        crea el rango de nodos (numero_nodos), determina el tipo de grafo si es dirigido o no dirigido (m_dirigido) y
+        creara el diccionario de la lista de adyacencia.
+
+
+        Parametros
+        ----------
+        m_numero_nodos : int
+            Cantidad de nodos que tendra el grafo.
+        m_nodos : int
+            Rango de nodos sobre los que trabajara el grafo.
+        m_dirigido : boolean
+            Tipo de nodo dirigido o no dirigido.
+        m_lista_adyacencia : diccionario
+            Diccionario que almacena el valor de los nodos
+        """
+        try:
+             # Se asigna el valor del numero de nodos a través del parametro recibido
+            self.m_numero_nodos = numero_nodos
+            # Se genera el rango de nodos en base a m_numero_nodos
+            self.m_nodos = range(self.m_numero_nodos)
+            # se define el tipo de grafo
+            self.m_dirigido = dirigido
+            
+            # Se crea el diccionario de la lista de adyacencia
+            self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
+        except Exception as e:  
+            print(e)
