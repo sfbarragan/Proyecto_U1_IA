09d11e312e8e9732d4d9f8e4ab074afb0d111e6e
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-08 19:14:51 -0500

    Se agrego los enlaces para cada estación

--- SistemaMetro.py
+++ SistemaMetro.py
@@ -1,5 +1,8 @@
 from queue import Queue
 
+from sklearn import tree
+from sqlalchemy import true
+
 
 class Grafo:
     """
@@ -175,24 +178,127 @@ if __name__ == "__main__":
     """
     Dentro de la clase main se instancia a la clase Grafo para acceder a sus metodos.
     """
-    #Diccionario
-    estaciones = {0:"Valdecarro",1:"Cuatro caminos",2:"Alto Moncloa",3:"Pinar de Chamartin",4:"Casa de campo",5:"Circular",6:"Hospital Henares Pitis",7:"Aeropuerto", 8:"Paco de Lucía", 9:"Hospital infante Sofia", 10:"Plaza Eliptica la fortuna", 11:"Metro sur", 12:"Opera", 13:"Las tablas", 14:"Aravaca", 15:"Puerta de Boadilla", 16:"San Ferrin-Oicasur", 17:"Estación de arte", 18:"Valdezarza", 19:"Lucero "}
-    
-    print("Caso de Prueba 1")
-    grafo1 = Grafo(5, estaciones)#Se instancia la clase Grafo
 
-    # Se agrega los bordes del grafo con valor peso = 1 por defecto
-    grafo1.crear_grafo(0, 2, 10)
-    grafo1.crear_grafo(0, 3, 10) 
-    grafo1.crear_grafo(0, 4, 25) 
+    try:
+
+        #Se crea el diccionario con las estaciones del tren
+        estaciones = {0:"Valdecarro",1:"Cuatro caminos",2:"Alto Moncloa",3:"Pinar de Chamartin",4:"Casa de campo",5:"Circular",6:"Hospital Henares Pitis",7:"Aeropuerto", 8:"Paco de Lucía", 9:"Hospital infante Sofia", 10:"Plaza Eliptica la fortuna", 11:"Metro sur", 12:"Opera", 13:"Las tablas", 14:"Aravaca", 15:"Puerta de Boadilla", 16:"San Ferrin-Oicasur", 17:"Estación de arte", 18:"Valdezarza", 19:"Lucero "}
+        
+        g = Grafo(20, estaciones)#Se instancia la clase Grafo
+
+        # Se agrega los bordes del grafo con valor peso correspondiente
+        g.crear_grafo(0, 1, 10)
+        g.crear_grafo(0, 2, 10) 
+        g.crear_grafo(0, 3, 25) 
+
+        g.crear_grafo(1, 3, 3)
+        g.crear_grafo(1, 2, 7)
+        g.crear_grafo(1, 6, 1)
+        g.crear_grafo(1, 8, 15)
+        g.crear_grafo(1, 4, 18)
+        g.crear_grafo(1, 5, 16)
+
+        g.crear_grafo(2, 3, 0.5)
+        g.crear_grafo(2, 1, 6)
+        g.crear_grafo(2, 9, 4)
+        g.crear_grafo(2, 0, 6)
+        g.crear_grafo(2, 4, 7)
+        g.crear_grafo(2, 5, 8)
+
+        g.crear_grafo(3, 1, 1)
+        g.crear_grafo(3, 0, 2.5) 
+        g.crear_grafo(3, 9, 3) 
+        g.crear_grafo(3, 6, 15) 
+        g.crear_grafo(3, 7, 19) 
+        
+        g.crear_grafo(4, 5, 5)
+        g.crear_grafo(4, 2, 8) 
+        g.crear_grafo(4, 9, 16)
+        g.crear_grafo(4, 8, 17) 
+        g.crear_grafo(4, 3, 18) 
+        g.crear_grafo(4, 1, 19) 
+        g.crear_grafo(4, 6, 20) 
+        
+        g.crear_grafo(5, 3, 0.5)
+        g.crear_grafo(5, 2, 0.9) 
+        g.crear_grafo(5, 1, 5) 
+        g.crear_grafo(5, 7, 6) 
+        g.crear_grafo(5, 6, 7) 
+        g.crear_grafo(5, 8, 7) 
+        g.crear_grafo(5, 4, 8) 
+        g.crear_grafo(5, 0, 15) 
+        g.crear_grafo(5, 10, 17) 
+        g.crear_grafo(5, 9, 30) 
+
+        #Aqui
+        g.crear_grafo(6, 5, 10)
+        g.crear_grafo(6, 1, 11) 
+        g.crear_grafo(6, 9, 12) 
+        g.crear_grafo(6, 8, 13) 
+        g.crear_grafo(6, 3, 13) 
+        g.crear_grafo(6, 4, 18)
+
+        g.crear_grafo(7, 3, 9)
+        g.crear_grafo(7, 8, 15)
+        g.crear_grafo(7, 5, 19)
+
+        g.crear_grafo(8, 0, 6)
+        g.crear_grafo(8, 7, 7)
+        g.crear_grafo(8, 6, 14)
+        g.crear_grafo(8, 3, 14)
+        g.crear_grafo(8, 4, 15)
+        g.crear_grafo(8, 1, 16)
+        g.crear_grafo(8, 5, 17.5)
+
+        g.crear_grafo(9, 14, 10)
+        g.crear_grafo(9, 4, 11)
+        g.crear_grafo(9, 5, 13)
+        g.crear_grafo(9, 2, 13.5)
+        g.crear_grafo(9, 0, 15)
+        g.crear_grafo(9, 4, 16)
+        g.crear_grafo(9, 6, 17)
+        g.crear_grafo(9, 7, 18)
+        g.crear_grafo(9, 13, 25)
+
+        g.crear_grafo(10, 5, 10)
+
+        g.crear_grafo(11, 9, 7)
+
+        g.crear_grafo(12, 9, 1)
+        g.crear_grafo(12, 4, 4)
+
+        g.crear_grafo(13, 9, 1)
+        g.crear_grafo(13, 0, 10)
+
+        g.crear_grafo(14, 2, 10)
+
+        g.crear_grafo(15, 1, 15)
+
+        g.crear_grafo(16, 2, 1)
+        
+        g.crear_grafo(17, 0, 1)
+
+        g.crear_grafo(18, 6, 5)
+
+        g.crear_grafo(19, 5, 2)
 
 
-    grafo1.crear_grafo(1, 3)
-    grafo1.crear_grafo(2, 3) 
-    grafo1.crear_grafo(3, 4) 
+        g.imprimir_grafo() #Imprime la lista de adyacencia
 
-    grafo1.imprimir_grafo() #Imprime la lista de adyacencia
+        ruta_transversal = [] # Se inicializa la variable ruta_transversal
+        
 
-    ruta_transversal1 = [] # Se inicializa la variable ruta_transversal
-    ruta_transversal1 = grafo1.dfs(0, 3) # Se almacena el recorrido dfs en la variable ruta_transversal
-    print(f"La ruta transversal desde el nodo 0 al nodo 3 es {ruta_transversal1}") #Imprime el recorriodo dfs
+        while true:
+            #Se pide al usuario la estación inicial
+            estacion_inicial = int(input("Ingrese la estación inicial")) 
+            #Se pide al usuario que ingrese la estación objetivo
+            estacion_objetivo = int(input("Ingrese la estación objetivo"))    
+            if(estacion_inicial >= 0 and estacion_inicial <= 19 and estacion_objetivo >= 0 and estacion_objetivo <= 19):
+                ruta_transversal = g.dfs(estacion_inicial, estacion_objetivo) # Se almacena el recorrido dfs en la variable ruta_transversal
+                print(f"La ruta para llegar al destino es:  {ruta_transversal}") #Imprime el recorriodo dfs
+                break
+            else:
+                print("Se ingreso una estación no válida")
+            
+    except Exception as e:
+            print(e)
