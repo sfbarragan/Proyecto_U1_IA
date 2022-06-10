a5be95288bc4d3e65696cf7380b2e5c9fff13ad6
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-09 22:36:27 -0500

    Se ajusto la clase test

--- test_SistemaMetro.py
+++ test_SistemaMetro.py
@@ -1,26 +1,26 @@
-from SistemaMetro import Grafo
 import unittest
+
+from SistemaMetro import Grafo
 
 
 class TestGrafo(unittest.TestCase):
 
-
     def test_grafo(self):
-
         """
         Este método testea el grafo.
         """
 
-        #Se crea el diccionario con las estaciones del tren
+        # Se crea el diccionario con las estaciones del tren
 
-        estaciones = {0: "Valdecarro",1: "Cuatro caminos",2:"Alto Moncloa",3:"Pinar de Chamartin",4:"Casa de campo",5:"Circular",6:"Hospital Henares Pitis",7:"Aeropuerto", 8:"Paco de Lucía", 9:"Hospital infante Sofia", 10:"Plaza Eliptica la fortuna", 11:"Metro sur", 12:"Opera", 13:"Las tablas", 14:"Aravaca", 15:"Puerta de Boadilla", 16:"San Ferrin-Oicasur", 17:"Estación de arte", 18:"Valdezarza", 19:"Lucero "}
+        estaciones = {0: "Valdecarro", 1: "Cuatro caminos", 2: "Alto Moncloa", 3: "Pinar de Chamartin", 4: "Casa de campo", 5: "Circular", 6: "Hospital Henares Pitis", 7: "Aeropuerto", 8: "Paco de Lucía", 9: "Hospital infante Sofia",
+                      10: "Plaza Eliptica la fortuna", 11: "Metro sur", 12: "Opera", 13: "Las tablas", 14: "Aravaca", 15: "Puerta de Boadilla", 16: "San Ferrin-Oicasur", 17: "Estación de arte", 18: "Valdezarza", 19: "Lucero "}
 
-        g = Grafo(20, estaciones)# Se instancia la clase Grafo
+        g = Grafo(20, estaciones)  # Se instancia la clase Grafo
 
         # Se agrega los bordes del grafo con valor peso correspondiente
         g.crear_grafo(0, 1, 10)
-        g.crear_grafo(0, 2, 10) 
-        g.crear_grafo(0, 3, 25) 
+        g.crear_grafo(0, 2, 10)
+        g.crear_grafo(0, 3, 25)
 
         g.crear_grafo(1, 3, 3)
         g.crear_grafo(1, 2, 7)
@@ -37,35 +37,35 @@ class TestGrafo(unittest.TestCase):
         g.crear_grafo(2, 5, 8)
 
         g.crear_grafo(3, 1, 1)
-        g.crear_grafo(3, 0, 2.5) 
-        g.crear_grafo(3, 9, 3) 
-        g.crear_grafo(3, 6, 15) 
-        g.crear_grafo(3, 7, 19) 
-        
+        g.crear_grafo(3, 0, 2.5)
+        g.crear_grafo(3, 9, 3)
+        g.crear_grafo(3, 6, 15)
+        g.crear_grafo(3, 7, 19)
+
         g.crear_grafo(4, 5, 5)
-        g.crear_grafo(4, 2, 8) 
+        g.crear_grafo(4, 2, 8)
         g.crear_grafo(4, 9, 16)
-        g.crear_grafo(4, 8, 17) 
-        g.crear_grafo(4, 3, 18) 
-        g.crear_grafo(4, 1, 19) 
-        g.crear_grafo(4, 6, 20) 
-        
+        g.crear_grafo(4, 8, 17)
+        g.crear_grafo(4, 3, 18)
+        g.crear_grafo(4, 1, 19)
+        g.crear_grafo(4, 6, 20)
+
         g.crear_grafo(5, 3, 0.5)
-        g.crear_grafo(5, 2, 0.9) 
-        g.crear_grafo(5, 1, 5) 
-        g.crear_grafo(5, 7, 6) 
-        g.crear_grafo(5, 6, 7) 
-        g.crear_grafo(5, 8, 7) 
-        g.crear_grafo(5, 4, 8) 
-        g.crear_grafo(5, 0, 15) 
-        g.crear_grafo(5, 10, 17) 
-        g.crear_grafo(5, 9, 30) 
+        g.crear_grafo(5, 2, 0.9)
+        g.crear_grafo(5, 1, 5)
+        g.crear_grafo(5, 7, 6)
+        g.crear_grafo(5, 6, 7)
+        g.crear_grafo(5, 8, 7)
+        g.crear_grafo(5, 4, 8)
+        g.crear_grafo(5, 0, 15)
+        g.crear_grafo(5, 10, 17)
+        g.crear_grafo(5, 9, 30)
 
         g.crear_grafo(6, 5, 10)
-        g.crear_grafo(6, 1, 11) 
-        g.crear_grafo(6, 9, 12) 
-        g.crear_grafo(6, 8, 13) 
-        g.crear_grafo(6, 3, 13) 
+        g.crear_grafo(6, 1, 11)
+        g.crear_grafo(6, 9, 12)
+        g.crear_grafo(6, 8, 13)
+        g.crear_grafo(6, 3, 13)
         g.crear_grafo(6, 4, 18)
 
         g.crear_grafo(7, 3, 9)
@@ -105,7 +105,7 @@ class TestGrafo(unittest.TestCase):
         g.crear_grafo(15, 1, 15)
 
         g.crear_grafo(16, 2, 1)
-        
+
         g.crear_grafo(17, 0, 1)
 
         g.crear_grafo(18, 6, 5)
@@ -114,13 +114,14 @@ class TestGrafo(unittest.TestCase):
 
         g.imprimir_grafo()
 
-        #Esta funciono
+        # Esta funciono
         #self.assertEqual(g.dfs(2, 7), ['Alto Moncloa', 'Circular', 'Cuatro caminos', 'Hospital Henares Pitis', 'Hospital infante Sofia', 'Casa de campo', 'Paco de Lucía', 'Pinar de Chamartin', 'Aeropuerto'])
-        
-        
+
         #self.assertEqual(g.dfs(0, 5), ['Valdecarro', 'Alto Moncloa', 'Circular'])
-        
+
         self.assertEqual(g.dfs(5, 10), ['Circular', 'Plaza Eliptica la fortuna'])
+
 
 if __name__ == '__main__':
     unittest.main(argv=['ignored', '-v'], exit=False)
+    
