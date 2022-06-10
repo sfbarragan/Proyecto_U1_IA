591fc22a0316e47f93702c06c908637f9ca0842b
MelanyL <mclopez13@espe.edu.ec>
2022-06-09 20:26:26 -0500

    Se establecio y desarrolló la HABITACIÓN 1

--- Puerta_Inteligente.py
+++ Puerta_Inteligente.py
@@ -6,6 +6,14 @@ from cmath import cos
 
 
 def puerta_Inteligente():
+    """
+    La función puerta inteligente nos permitirá ingresar a las distintas ubicaciones, las cuales son:
+    HABITACION1, HABITACION2, HABITACION3, HABITACION4, HABITACION5, HABITACION6, HABITACION7.
+    
+    Tomando en cuenta que para poder acceder a las distintas Habitaciones establecidas se de tener la llave de acceso dentro del rango, 
+    y dependiendo del estado que se encuentre se puede acceder a dicha habitacion.
+    Nota: El estado objetivo del agente puerta inteligente es el estado abierto. 
+    """
     # inicializamos estadoObjetivo
     # 0 idica abrir y 1 indica cerrar
     estadoObjetivo = {'HABITACION1': '0', 'HABITACION2': '0', 'HABITACION3': '0',
@@ -40,7 +48,7 @@ def puerta_Inteligente():
 
             if(estado == '1'):
                 # La puerta Inteligente de la HABITACION1 se encuentra cerrada
-                print("La puerta Inteligente se encuentra cerrada")
+                print("La puerta Inteligente de la HABITACION1 se encuentra cerrada")
                 # La puerta Inteligente empezará abrirse
                 estadoObjetivo['HABITACION1'] = '0'
                 costo += 1  # El costo incrementará cada que la puerta Inteligente se abra
@@ -85,9 +93,8 @@ def puerta_Inteligente():
 
                 if (llave == 'si'):
                     if(ubicacion3 == '1'):
-                        # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
-                        print(
-                            "La puerta Inteligente de la HABITACION 3 se encuenta cerrada")
+                        # Si la puerta inteligente de la HABITACION3 se encuentra cerrado.
+                        print("La puerta Inteligente de la HABITACION 3 se encuenta cerrada")
                         print("El usuario se dirige a la siguiente HABITACION")
                         costo += 1  # Incrementa el costo por moverse de locacion
                         # Muestra el costo actual
@@ -95,16 +102,13 @@ def puerta_Inteligente():
                         # La puerta inteligente se abrirá
                         estadoObjetivo['HABITACION3'] = '0'
                         costo += 1  # El costo incrementará cada que la puerta inteligente se abra
-                        print(
-                            "La puerta inteligente se encuentra abierta en la HABITACION 3")
+                        print("La puerta inteligente se encuentra abierta en la HABITACION 3")
                         # Muestra el costo actual
                         print("El costo actual es: " + str(costo))
                     else:
                         # La puerta inteligente se encuentra abierta
-                        print(
-                            "La puerta Inteligente de la HABITACION 3 se encuentra abierta")
-                        print(
-                            "No se lleva a cabo ninguna acción. El costo actual es: " + str(costo))
+                        print("La puerta Inteligente de la HABITACION 3 se encuentra abierta")
+                        print("No se lleva a cabo ninguna acción. El costo actual es: " + str(costo))
 
                 elif (llave == 'no'):
                     print("No se encuentra dentro del rango establecido!")
@@ -117,9 +121,8 @@ def puerta_Inteligente():
 
                 if (llave == 'si'):
                     if(ubicacion4 == '1'):
-                        # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
-                        print(
-                            "La puerta Inteligente de la HABITACION 4 se encuenta cerrada")
+                        # Si la puerta inteligente de la HABITACION4 se encuentra cerrado.
+                        print("La puerta Inteligente de la HABITACION 4 se encuenta cerrada")
                         print("El usuario se dirige a la siguiente HABITACION")
                         costo += 1  # Incrementa el costo por moverse de locacion
                         # Muestra el costo actual
@@ -133,10 +136,8 @@ def puerta_Inteligente():
                         print("El costo actual es: " + str(costo))
                     else:
                         # La puerta inteligente se encuentra abierta
-                        print(
-                            "La puerta Inteligente de la HABITACION 4 se encuentra abierta")
-                        print(
-                            "No se lleva a cabo ninguna acción. El costo actual es: " + str(costo))
+                        print("La puerta Inteligente de la HABITACION 4 se encuentra abierta")
+                        print("No se lleva a cabo ninguna acción. El costo actual es: " + str(costo))
 
                 elif (llave == 'no'):
                     print("No se encuentra dentro del rango establecido!")
@@ -149,9 +150,8 @@ def puerta_Inteligente():
 
                 if (llave == 'si'):
                     if(ubicacion5 == '1'):
-                        # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
-                        print(
-                            "La puerta Inteligente de la HABITACION 5 se encuenta cerrada")
+                        # Si la puerta inteligente de la HABITACION5 se encuentra cerrado.
+                        print("La puerta Inteligente de la HABITACION 5 se encuenta cerrada")
                         print("El usuario se dirige a la siguiente HABITACION")
                         costo += 1  # Incrementa el costo por moverse de locacion
                         # Muestra el costo actual
@@ -159,16 +159,13 @@ def puerta_Inteligente():
                         # La puerta inteligente se abrirá
                         estadoObjetivo['HABITACION5'] = '0'
                         costo += 1  # El costo incrementará cada que la puerta inteligente se abra
-                        print(
-                            "La puerta inteligente se encuentra abierta en la HABITACION 5")
+                        print("La puerta inteligente se encuentra abierta en la HABITACION 5")
                         # Muestra el costo actual
                         print("El costo actual es: " + str(costo))
                     else:
                         # La puerta inteligente se encuentra abierta
-                        print(
-                            "La puerta Inteligente de la HABITACION 5 se encuentra abierta")
-                        print(
-                            "No se lleva a cabo ninguna acción. El costo actual es: " + str(costo))
+                        print("La puerta Inteligente de la HABITACION 5 se encuentra abierta")
+                        print("No se lleva a cabo ninguna acción. El costo actual es: " + str(costo))
 
                 elif (llave == 'no'):
                     print("No se encuentra dentro del rango establecido!")
@@ -181,7 +178,7 @@ def puerta_Inteligente():
 
                 if (llave == 'si'):
                     if(ubicacion6 == '1'):
-                        # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                        # Si la puerta inteligente de la HABITACION6 se encuentra cerrado.
                         print(
                             "La puerta Inteligente de la HABITACION 6 se encuenta cerrada")
                         print("El usuario se dirige a la siguiente HABITACION")
@@ -213,7 +210,7 @@ def puerta_Inteligente():
 
                 if (llave == 'si'):
                     if(ubicacion7 == '1'):
-                        # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                        # Si la puerta inteligente de la HABITACION7 se encuentra cerrado.
                         print(
                             "La puerta Inteligente de la HABITACION 7 se encuenta cerrada")
                         print("El usuario se dirige a la siguiente HABITACION")
@@ -278,7 +275,7 @@ def puerta_Inteligente():
 
         if (llave == 'si'):
             if(ubicacion3 == '1'):
-                # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                # Si la puerta inteligente de la HABITACION3 se encuentra cerrado.
                 print("La puerta Inteligente de la HABITACION 3 se encuenta cerrada")
                 print("El usuario se dirige a la siguiente HABITACION")
                 costo += 1 #Incrementa el costo por moverse de locacion
@@ -304,7 +301,7 @@ def puerta_Inteligente():
 
             if (llave == 'si'):
                 if(ubicacion4 == '1'):
-                    # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                    # Si la puerta inteligente de la HABITACION4 se encuentra cerrado.
                     print("La puerta Inteligente de la HABITACION 4 se encuenta cerrada")
                     print("El usuario se dirige a la siguiente HABITACION")
                     costo += 1 #Incrementa el costo por moverse de locacion
@@ -330,7 +327,7 @@ def puerta_Inteligente():
 
             if (llave == 'si'):
                 if(ubicacion5 == '1'):
-                    # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                    # Si la puerta inteligente de la HABITACION5 se encuentra cerrado.
                     print("La puerta Inteligente de la HABITACION 5 se encuenta cerrada")
                     print("El usuario se dirige a la siguiente HABITACION")
                     costo += 1 #Incrementa el costo por moverse de locacion
@@ -356,7 +353,7 @@ def puerta_Inteligente():
 
             if (llave == 'si'):
                 if(ubicacion6 == '1'):
-                    # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                    # Si la puerta inteligente de la HABITACION6 se encuentra cerrado.
                     print("La puerta Inteligente de la HABITACION 6 se encuenta cerrada")
                     print("El usuario se dirige a la siguiente HABITACION")
                     costo += 1 #Incrementa el costo por moverse de locacion
@@ -382,7 +379,7 @@ def puerta_Inteligente():
 
             if (llave == 'si'):
                 if(ubicacion7 == '1'):
-                    # Si la puerta inteligente de la HABITACION2 se encuentra cerrado.
+                    # Si la puerta inteligente de la HABITACION7 se encuentra cerrado.
                     print("La puerta Inteligente de la HABITACION 7 se encuenta cerrada")
                     print("El usuario se dirige a la siguiente HABITACION")
                     costo += 1 #Incrementa el costo por moverse de locacion
@@ -403,34 +400,3 @@ def puerta_Inteligente():
             else:
                 print("Opción Inválida!!!")
 
-
-        if (ubicacion == 'HABITACION2'):
-            # el usuario indica la si la puerta inteligente se encuentra abierto o cerrado (0/1) en la ubicación indicada.
-            ubicacion2 = input("Inserte el estado de la HABITACION 2 ")
-            # el usuario indica la si la puerta inteligente se encuentra abierto o cerrado (0/1) en la ubicación indicada.
-            ubicacion3 = input("Inserte el estado de la HABITACION 3 ")
-            # el usuario indica la si la puerta inteligente se encuentra abierto o cerrado (0/1) en la ubicación indicada.
-            ubicacion4 = input("Inserte el estado de la HABITACION 4 ")
-            # el usuario indica la si la puerta inteligente se encuentra abierto o cerrado (0/1) en la ubicación indicada.
-            ubicacion5 = input("Inserte el estado de la HABITACION 5 ")
-            # el usuario indica la si la puerta inteligente se encuentra abierto o cerrado (0/1) en la ubicación indicada.
-            ubicacion6 = input("Inserte el estado de la HABITACION 6 ")
-            # el usuario indica la si la puerta inteligente se encuentra abierto o cerrado (0/1) en la ubicación indicada.
-            ubicacion7 = input("Inserte el estado de la HABITACION 7 ")
-
-            # Se muestran los resultados deseados.
-            print("Objetivo Deseado:" + str(estadoObjetivo))
-            # Se muestra la habitación en la que se encuentra la puerta Inteligente
-            print("La Puerta Inteligente se encuentra en la HABITACION 1")
-
-            if(estado == '1'):
-                # La puerta Inteligente de la HABITACION1 se encuentra cerrada
-                print("La puerta Inteligente se encuentra cerrada")
-                # La puerta Inteligente empezará abrirse
-                estadoObjetivo['HABITACION1'] = '0'
-                costo += 1  # El costo incrementará cada que la puerta Inteligente se abra
-                print("La puerta Inteligente esta abierta en la HABITACION 1")
-                # Muestra el costo actual.
-                print("El costo actual es: " + str(costo))
-                llave = input(
-                    "La llave se encuentra dentro del rango (si o no): ")
