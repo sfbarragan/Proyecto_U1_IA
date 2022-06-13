ebb45a54e9e08f0012f4ea1572bcebbf47e1c81e
MelanyL <mclopez13@espe.edu.ec>
2022-06-12 23:06:33 -0500

    Se importan las respectivas librerías y se importa la clase puertaInteligente al respectivo test.

---
+++ test_Agente_PuertaInteligente.py
@@ -0,0 +1,6 @@
+# Se importa la libreria unittest
+import unittest
+
+# Se importa la clase puertaInteligente de Agente_PuertaInteligente.py
+from Agente_PuertaInteligente import puertaInteligente
+
