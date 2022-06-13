4c7881f151c731bb1834079a4312d8141b892e4f
MelanyL <mclopez13@espe.edu.ec>
2022-06-12 22:10:17 -0500

    Se reestructuro el modulo Puerta_Inteligente, creando un nuevo modulo Agente_PuertaInteligente.py. Con su respectiva clase.

---
+++ Agente_PuertaInteligente.py
@@ -0,0 +1,10 @@
+class puertaInteligente:
+    """
+    Esta clase representa a una puerta inteligente la cual tiene un estado, una ubicacion, una llave y un costo, a traves de los cuales se puede abrir o cerrar la puerta.
+    
+    Métodos
+    ----------
+    definirHabitacion(self,ubicacion, estado, costo, llave, estadoObjetivo)
+        Este método define la ubicacion, el estado, el costo y la llave de la puerta inteligente, estos parametros serviran para realizar el análisis correspondiente para saber si se debe abrir o no la puerta.
+    """
+    
