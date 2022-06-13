9eae8b24d28fb9a459ffbe630a852de242eccce9
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-13 18:03:25 -0500

    Se agrego pruebas automáticas

--- test_Agente_PuertaInteligente.py
+++ test_Agente_PuertaInteligente.py
@@ -12,9 +12,9 @@ class TestPuertaInteligente(unittest.TestCase):
         test_puerta(): Este método testea la puerta inteligente.
     """
 
-    def test_puerta(self):
+    def test_puerta_valoresCorrectos(self):
         """
-        Este método testea la puerta inteligente.
+        Este método testea la puerta inteligente con valores correctos.
         """
 
         # Se crea el diccionario de la ubicacion de la puerta inteligente.
@@ -29,6 +29,26 @@ class TestPuertaInteligente(unittest.TestCase):
         self.assertEqual(puerta.definirHabitacion('HABITACION3', '0', 0, 'SI', estadoObjetivo), 0)
         # Se agrega el estado la ubicación, estado, costo, estado de la llave y ubicaciones de la puerta inteligente.
         self.assertEqual(puerta.definirHabitacion('HABITACION7', '1', 0, 'NO', estadoObjetivo), 0)
+        
+    def test_puerta_valoresErroneos(self):
+        """
+        Este método testea la puerta inteligente con valores erroneos.
+        """
+
+        # Se crea el diccionario de la ubicacion de la puerta inteligente.
+        estadoObjetivo = {'HABITACION1': '0', 'HABITACION2': '0', 'HABITACION3': '0','HABITACION4': '0', 'HABITACION5': '0', 'HABITACION6': '0', 'HABITACION7': '0'}
+        
+        # Se crea una instancia de la clase puertaInteligente.
+        puerta = puertaInteligente() 
+
+        # Se agrega el estado la ubicación, estado, costo, estado de la llave y ubicaciones de la puerta inteligente.
+        self.assertFalse(puerta.definirHabitacion('HABITACION1', '1', 0, 'A', estadoObjetivo), 1)
+        # Se agrega el estado la ubicación, estado, costo, estado de la llave y ubicaciones de la puerta inteligente.
+        self.assertFalse(puerta.definirHabitacion('HABITACION3', '6', 0, 'SI', estadoObjetivo), 0)
+        # Se agrega el estado la ubicación, estado, costo, estado de la llave y ubicaciones de la puerta inteligente.
+        self.assertFalse(puerta.definirHabitacion('HABITACION7', '1', 0, 'NO', estadoObjetivo), 6)
+        
+        
 
 # Ejecucion de las pruebas. 
 if __name__ == '__main__':
--- test_SistemaMetro.py
+++ test_SistemaMetro.py
@@ -3,6 +3,7 @@ import unittest
 
 # Se importa la clase Grafo de SistemaMetro.py
 from SistemaMetro import Grafo
+from SistemaMetro import llenar_grafo
 
 
 class TestGrafo(unittest.TestCase):
@@ -13,9 +14,9 @@ class TestGrafo(unittest.TestCase):
         test_grafo(): Este método testea el grafo.
     """
 
-    def test_grafo(self):
+    def test_grafo_datosCorrectos(self):
         """
-        Este método testea el grafo.
+        Este método testea la clase Grafo con datos correctos.
         """
 
         # Se crea el diccionario con las estaciones del tren
@@ -25,7 +26,7 @@ class TestGrafo(unittest.TestCase):
         g = Grafo(20, estaciones)  
 
         # Se agrega los bordes del grafo con valor peso correspondiente
-        g.llenar_grafo(g)
+        llenar_grafo(g)
 
         # Se agrega las estaciones sobre las cuales se realizará el recorrido del metro a ser evaluado
         self.assertEqual(g.dfs(2, 7, ruta = [], visitado = set()), ['Alto Moncloa', 'Circular', 'Cuatro caminos', 'Hospital Henares Pitis', 'Hospital infante Sofia', 'Casa de campo', 'Paco de Lucía', 'Pinar de Chamartin', 'Aeropuerto'])
@@ -33,6 +34,33 @@ class TestGrafo(unittest.TestCase):
         self.assertEqual(g.dfs(0, 5, ruta = [], visitado = set()), ['Valdecarro', 'Alto Moncloa', 'Circular'])
         # Se agrega las estaciones sobre las cuales se realizará el recorrido del metro a ser evaluado
         self.assertEqual(g.dfs(5, 10, ruta = [], visitado = set()), ['Circular', 'Plaza Eliptica la fortuna'])
+
+
+    def test_grafo_datosErroneos(self):
+        """
+        Este método testea la clase Grafo con datos erroneos.
+        """
+
+        # Se crea el diccionario con las estaciones del tren
+
+        estaciones = {0: "Valdecarro", 1: "Cuatro caminos", 2: "Alto Moncloa", 3: "Pinar de Chamartin", 4: "Casa de campo", 5: "Circular", 6: "Hospital Henares Pitis", 7: "Aeropuerto", 8: "Paco de Lucía", 9: "Hospital infante Sofia", 10: "Plaza Eliptica la fortuna", 11: "Metro sur", 12: "Opera", 13: "Las tablas", 14: "Aravaca", 15: "Puerta de Boadilla", 16: "San Ferrin-Oicasur", 17: "Estación de arte", 18: "Valdezarza", 19: "Lucero "}
+        # Se instancia la clase Grafo
+        g = Grafo(20, estaciones)  
+
+        # Se agrega los bordes del grafo con valor peso correspondiente
+        llenar_grafo(g)
+
+        # Se agrega las estaciones sobre las cuales se realizará el recorrido del metro a ser evaluado
+        self.assertFalse(g.dfs(2, 21, ruta = [], visitado = set()), ['Alto Moncloa', 'Circular', 'Cuatro caminos', 'Hospital Henares Pitis', 'Hospital infante Sofia', 'Casa de campo', 'Paco de Lucía', 'Pinar de Chamartin', 'Aeropuerto'])
+        # Se agrega las estaciones sobre las cuales se realizará el recorrido del metro a ser evaluado
+        self.assertFalse(g.dfs(-1, 5, ruta = [], visitado = set()), ['Valdecarro', 'Alto Moncloa', 'Circular'])
+        # Se agrega las estaciones sobre las cuales se realizará el recorrido del metro a ser evaluado
+        self.assertFalse(g.dfs(22, 10, ruta = [], visitado = set()), ['Circular', 'Plaza Eliptica la fortuna'])
+
+
+
+
+
 
 # Ejecucion de las pruebas.
 if __name__ == '__main__':
