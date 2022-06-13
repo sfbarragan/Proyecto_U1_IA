93cdee08c84dc9b510d4866d0d84b576298b29a4
MelanyL <mclopez13@espe.edu.ec>
2022-06-12 22:21:09 -0500

    Se establecio el método definirHabitación, con sus respectivos parámetros que se implementarán dentro del mismo.

--- Agente_PuertaInteligente.py
+++ Agente_PuertaInteligente.py
@@ -8,3 +8,17 @@ class puertaInteligente:
         Este método define la ubicacion, el estado, el costo y la llave de la puerta inteligente, estos parametros serviran para realizar el análisis correspondiente para saber si se debe abrir o no la puerta.
     """
     
+    def definirHabitacion(self,ubicacion, estado, costo, llave, estadoObjetivo):
+        """_summary_
+
+        Parametros
+        -----------
+            ubicacion (str): Indica la ubicacion de la puerta inteligente.
+            estado (str): Indica el estado de la puerta inteligente.
+            costo (int): Indica el costo de las acciones realizadas por la puerta inteligente.
+            llave (str): Describe si la llave se encuentra dentro del rango de accion de la puerta inteligente.
+            estadoObjetivo (diccionario): Proporciona el estado objetivo de las habitaciones evaluadas.
+
+        Returns:
+            costo: Retorna el costo total de las acciones realizadas por la puerta inteligente.
+        """
